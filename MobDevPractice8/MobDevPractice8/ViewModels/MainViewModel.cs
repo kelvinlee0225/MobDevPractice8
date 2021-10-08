c7a5428165d384a62295672eb7be1c384b3e932f
kelvinlee0225 <leekelvin0225@gmail.com>
2021-10-07 21:57:11 -0400

    Add Service Dependecy

---
+++ MobDevPractice8/MobDevPractice8/ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using MobDevPractice8.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace MobDevPractice8.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        IDeviceOrientationService service = DependencyService.Get<IDeviceOrientationService>();
+        public DeviceOrientation orientation { get; set; }
+        public string Orientation { get; set; }
+        public MainViewModel()
+        {
+            DeviceOrientation orientation = service.GetOrientation();
+            Orientation = orientation.ToString();
+        }
+    }
+}
